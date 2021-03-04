36c72322030ce01f3340c5ada345e289fd622242
Kellphy <virtual_44@yahoo.com>
2021-03-04 03:14:54 +0200

    v0.1.0

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -248,7 +248,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Kellphy\'s Emotes Everywhere";
+            this.Text = "Kellphy\'s Emotes Everywhere [kellphy.com/kee]";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
