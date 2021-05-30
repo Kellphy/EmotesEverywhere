18ae1628ef7b48da53788ce788082b284a39e407
Kellphy <virtual_44@yahoo.com>
2021-05-30 22:56:51 +0300

    0.4.0

--- Form2.cs
+++ Form2.cs
@@ -18,6 +18,7 @@ namespace KEE
             label1.Text = "# Find emotes by entering their full name in the search box," +
                 "\n clicking the search results or drag-and-drop with RMB (best)." +
                 "\n# Press [Win+V] to see the clipboard history." +
+                "\n# Search \"gif\" for GIFs and \"png\" for PNGs." +
                 "\n" +
                 "\n# The 1st option is the default one, but you are free to try any of the 3:" +
                 "\n[Option 1] - The image's background will be Discord's dark theme color." +
